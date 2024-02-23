bb5a4eafa6faeb577c747bc7d7f186f5b3b3fc6b
AtulRaghuvanshi73 <atulraghuvanshi09@gmail.com>
2024-02-23 22:18:47 +0530

    initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import {logo} from '../assets/assets'
+
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+        <nav className='flex justify-between items-center flex-col'>
+            <img src= {logo} alt="hero_img"
+            className='w-28 object-contain'/>
+
+            <button 
+                type="button"
+                onClick={() => window.open('')}
+            >
+                Github
+            </button>
+        </nav>
+    </header>
+  )
+}
+
+export default Hero
